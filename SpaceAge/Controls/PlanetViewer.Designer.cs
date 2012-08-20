7c08f363cab48de375bc00ec58ce12515b7b568e
matthewpapa <matthewpapa07@users.noreply.github.com>
2012-08-20 21:37:57 +0000

    More changes to PlanetViewer

--- SpaceAge/Controls/PlanetViewer.Designer.cs
+++ SpaceAge/Controls/PlanetViewer.Designer.cs
@@ -34,17 +34,21 @@
             this.listview_elementResources = new System.Windows.Forms.ListView();
             this.asfgfsagafsg = new System.Windows.Forms.Label();
             this.listview_atmosphericGas = new System.Windows.Forms.ListView();
+            this.listview_naturalResources = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // sdgsdgasgasfg
             // 
             this.sdgsdgasgasfg.AutoSize = true;
-            this.sdgsdgasgasfg.Location = new System.Drawing.Point(257, 6);
+            this.sdgsdgasgasfg.Location = new System.Drawing.Point(257, 131);
             this.sdgsdgasgasfg.Name = "sdgsdgasgasfg";
             this.sdgsdgasgasfg.Size = new System.Drawing.Size(99, 13);
             this.sdgsdgasgasfg.TabIndex = 0;
-            this.sdgsdgasgasfg.Text = "Element Resources";
+            this.sdgsdgasgasfg.Text = "Abundant Elements";
             // 
             // panel1
             // 
@@ -65,16 +69,16 @@
             // 
             // listview_elementResources
             // 
-            this.listview_elementResources.Location = new System.Drawing.Point(260, 22);
+            this.listview_elementResources.Location = new System.Drawing.Point(260, 147);
             this.listview_elementResources.Name = "listview_elementResources";
-            this.listview_elementResources.Size = new System.Drawing.Size(233, 97);
+            this.listview_elementResources.Size = new System.Drawing.Size(318, 97);
             this.listview_elementResources.TabIndex = 2;
             this.listview_elementResources.UseCompatibleStateImageBehavior = false;
             // 
             // asfgfsagafsg
             // 
             this.asfgfsagafsg.AutoSize = true;
-            this.asfgfsagafsg.Location = new System.Drawing.Point(257, 131);
+            this.asfgfsagafsg.Location = new System.Drawing.Point(257, 13);
             this.asfgfsagafsg.Name = "asfgfsagafsg";
             this.asfgfsagafsg.Size = new System.Drawing.Size(87, 13);
             this.asfgfsagafsg.TabIndex = 3;
@@ -82,23 +86,62 @@
             // 
             // listview_atmosphericGas
             // 
-            this.listview_atmosphericGas.Location = new System.Drawing.Point(260, 147);
+            this.listview_atmosphericGas.Location = new System.Drawing.Point(260, 29);
             this.listview_atmosphericGas.Name = "listview_atmosphericGas";
-            this.listview_atmosphericGas.Size = new System.Drawing.Size(233, 97);
+            this.listview_atmosphericGas.Size = new System.Drawing.Size(318, 97);
             this.listview_atmosphericGas.TabIndex = 4;
             this.listview_atmosphericGas.UseCompatibleStateImageBehavior = false;
+            // 
+            // listview_naturalResources
+            // 
+            this.listview_naturalResources.Location = new System.Drawing.Point(260, 267);
+            this.listview_naturalResources.Name = "listview_naturalResources";
+            this.listview_naturalResources.Size = new System.Drawing.Size(318, 97);
+            this.listview_naturalResources.TabIndex = 5;
+            this.listview_naturalResources.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Natural Resources";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Planet Position:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Planet Size:";
             // 
             // PlanetViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listview_naturalResources);
             this.Controls.Add(this.listview_atmosphericGas);
             this.Controls.Add(this.asfgfsagafsg);
             this.Controls.Add(this.listview_elementResources);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.sdgsdgasgasfg);
             this.Name = "PlanetViewer";
-            this.Size = new System.Drawing.Size(505, 263);
+            this.Size = new System.Drawing.Size(591, 375);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -114,5 +157,9 @@
         private System.Windows.Forms.ListView listview_elementResources;
         private System.Windows.Forms.Label asfgfsagafsg;
         private System.Windows.Forms.ListView listview_atmosphericGas;
+        private System.Windows.Forms.ListView listview_naturalResources;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
