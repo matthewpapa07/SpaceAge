d7c9d0586813da5ce2a088d69730742c1cf3f25c
matthewpapa <matthewpapa07@users.noreply.github.com>
2012-08-22 02:43:40 +0000

    changes to RawMaterialExtractor

--- SpaceAge/RawMaterialExtractor.cs
+++ SpaceAge/RawMaterialExtractor.cs
@@ -7,16 +7,27 @@ namespace SpaceAge
 {
     class RawMaterialExtractor
     {
-        /*
-        Fuel = 0, Water, Uranium, Foodstuffs, RepairPatch, ScrapMetal, Coolant, CopperCabling,
-        Slaves, Minerals, PreciousMetals, SpaceSteel, Hydrogen, Titanium, Ceramics, Spices,
-        ComputerComponents, Biomass, Hydrocarbons,
-        Missile1, Missile2, LaserCrystal1, LaserCrystal2, MassDriverAmmo1, MassDriverAmmo2
-         * */
+        //
+        //Fuel = 0, Water, Foodstuffs, RepairPatch, ScrapMetal, Coolant, CopperCabling,
+        //Slaves, Minerals, SpaceSteel, Ceramics, Spices,
+        //ComputerComponents, Missile1, Missile2, LaserCrystal1, LaserCrystal2, 
+        //MassDriverAmmo1, MassDriverAmmo2,
+        //// RESOURCES BEGIN HERE
+        //Oxygen, Hydrogen, Methane, SulphuricAcid, CarbonDioxide, Nitrogen, Chlorine, Helium,    // CommonAtmosphere
+        //Boron, Neon, Xenon, Krypton,                                                            // RareAtmosphere
+        //Silicon, Iron, Carbon, Copper, Magnesium, Sodium, Sulfur, Lead, Nickel, Amuninum,       // CommonElements
+        //Titanium, Neodymium, Germanium, Gallium, Arsenic, Strontium, Gold, Silver, Platinum,    // RareElements
+        //Hydrocarbons, Cellulose, Acid, Biomass, Mud                                             // ResourcesStatic
+        //
+
         Commodity.CommodityEnum ProducedCommodities;
 
         public RawMaterialExtractor(Planet p)
         {
+            // For now do not allow inhabited planets to be exploited
+            if (p.IsInhabited)
+                throw new Exception();
+
 
         }
 
