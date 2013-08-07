6438cc9c2dd8f2b700acc06fab056731ed995fcc
matthewpapa <matthewpapa@52d4c13d-ea8d-4f82-9d3e-ff18ec58a77f>
2013-08-07 17:35:19 +0000

    Planets now work! They are really ugly for now but that is ok.

--- trunk/SpaceAge/SpaceAge/CelestialBody/Star.cs
+++ trunk/SpaceAge/SpaceAge/CelestialBody/Star.cs
@@ -142,25 +142,25 @@ namespace SpaceAge
             return DrawStarGraphics();
         }
 
-    }
-
-
-    static class StarConstant
-    {
-        //
-        // Commonly Associated with Stars
-        //
-        public static int StarTypes = 10;
-        public enum StarClass { O, B, A, F, G, K, M, L, T, Y };       // Ten total star classifications
-        public static string[] StarClassStr = { "XXXL", "XXL", "XL", "L", "M", "S", "XS", "XXS", "XXS", "XXS" };
-        public static double[] StarP = { 0.03, 0.07, 0.07, 0.13, 0.15, 0.2, 0.3, 0.03, 0.01, 0.01 };    // These numbers MUST total to 1
-        public static Color[] BaseColor = { Color.FromArgb(0x00, 0xbf, 0xff), Color.FromArgb(0x87, 0xce, 0xeb), Color.FromArgb(0xb0, 0xe0, 0xe6),
-                                            Color.FromArgb(0xf8, 0xf8, 0xff), Color.FromArgb(0xff, 0xff, 0x00), Color.FromArgb(0xff, 0xa5, 0x00),
-                                            Color.FromArgb(0xff, 0x00, 0x00), Color.FromArgb(0xb2, 0x22, 0x22), Color.FromArgb(0xcd, 0x85, 0x3f),
-                                            Color.FromArgb(0x8b, 0x45, 0x13) };
-        public static double[] MassMin = { 16.0, 2.1, 1.4, 1.04, 0.8, 0.45, 0.30, 0.20, 0.10, 0.05 };
-        public static double[] MassMax = { 30.0, 16.0, 2.1, 1.4, 1.04, 0.8, 0.45, 0.30, 0.20, 0.10 };
+        public static class StarConstant
+        {
+            //
+            // Commonly Associated with Stars
+            //
+            public static int StarTypes = 10;
+            public enum StarClass { O, B, A, F, G, K, M, L, T, Y };       // Ten total star classifications
+            public static string[] StarClassStr = { "XXXL", "XXL", "XL", "L", "M", "S", "XS", "XXS", "XXS", "XXS" };
+            public static double[] StarP = { 0.03, 0.07, 0.07, 0.13, 0.15, 0.2, 0.3, 0.03, 0.01, 0.01 };    // These numbers MUST total to 1
+            public static Color[] BaseColor = { Color.FromArgb(0x00, 0xbf, 0xff), Color.FromArgb(0x87, 0xce, 0xeb), Color.FromArgb(0xb0, 0xe0, 0xe6),
+                                                Color.FromArgb(0xf8, 0xf8, 0xff), Color.FromArgb(0xff, 0xff, 0x00), Color.FromArgb(0xff, 0xa5, 0x00),
+                                                Color.FromArgb(0xff, 0x00, 0x00), Color.FromArgb(0xb2, 0x22, 0x22), Color.FromArgb(0xcd, 0x85, 0x3f),
+                                                Color.FromArgb(0x8b, 0x45, 0x13) };
+            public static double[] MassMin = { 16.0, 2.1, 1.4, 1.04, 0.8, 0.45, 0.30, 0.20, 0.10, 0.05 };
+            public static double[] MassMax = { 30.0, 16.0, 2.1, 1.4, 1.04, 0.8, 0.45, 0.30, 0.20, 0.10 };
         
-        public static int[] StarDiameter = { 350, 300, 280, 205, 220, 190, 180, 170, 160, 150 };
+            public static int[] StarDiameter = { 350, 300, 280, 205, 220, 190, 180, 170, 160, 150 };
+        }
+
     }
+
 }
